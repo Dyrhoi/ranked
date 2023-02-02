a3732ccfb7fafcdadddbd6444c8c173f7477cd48
Dyrhoi <realarcoz@gmail.com>
2023-02-02 23:15:35 +0100

    fix: order leaderboard by elo.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,8 +3,10 @@ import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
 	return {
-		players: await db.player.findMany({
-			include: { eloInstants: { orderBy: { createdAt: 'desc' } } },
-		}),
+		players: (
+			await db.player.findMany({
+				include: { eloInstants: { orderBy: { createdAt: 'desc' } } },
+			})
+		).sort((a, b) => b.eloInstants[0].elo - a.eloInstants[0].elo),
 	};
 }) satisfies PageServerLoad;
