bb83dd2c3279d883e0d682931db94f9fed72ad59
Dyrhoi <realarcoz@gmail.com>
2023-02-22 21:06:44 +0100

    chore: SCSS support. feat: Some glow for hovering cards (temp ?)

--- src/lib/web/card/Card.svelte
+++ src/lib/web/card/Card.svelte
@@ -8,25 +8,52 @@
 <div
 	class={classnames(
 		'bg-gradient-to-t from-white/5 to-white/0 card p-24 rounded-xl relative overflow-hidden',
-		clazz
+		clazz,
+		'after:'
 	)}
 >
 	<slot />
 </div>
 
-<style>
+<style lang="scss">
 	/* Linear Stroke */
-	.card::before {
-		content: '';
-		pointer-events: none;
-		user-select: none;
-		position: absolute;
-		inset: 0px;
-		border-radius: inherit;
-		padding: 1px;
-		background: linear-gradient(rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.07));
-		-webkit-mask: linear-gradient(black, black) content-box content-box,
-			linear-gradient(black, black);
-		-webkit-mask-composite: xor;
+	.card {
+		&::after,
+		&::before {
+			content: '';
+			pointer-events: none;
+			user-select: none;
+			position: absolute;
+			inset: 0px;
+			border-radius: inherit;
+		}
+
+		&::before {
+			padding: 1px;
+			background: linear-gradient(rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.07));
+			-webkit-mask: linear-gradient(black, black) content-box content-box,
+				linear-gradient(black, black);
+			-webkit-mask-composite: xor;
+		}
+
+		&::after {
+			transition: all 0.2s;
+			background-position: left;
+			background: linear-gradient(
+				to bottom right,
+				rgba(255, 255, 255, 0) 30%,
+				rgba(255, 255, 255, 0.02),
+				rgba(255, 255, 255, 0) 70%
+			);
+			background-size: 100% 200%;
+			opacity: 0;
+		}
+
+		&:hover {
+			&::after {
+				background-position: right;
+				opacity: 1;
+			}
+		}
 	}
 </style>
