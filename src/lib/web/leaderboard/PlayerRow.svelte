91e93fb9762449cd472935f7f4bb8d761f6feaa4
Dyrhoi <realarcoz@gmail.com>
2023-03-06 22:34:32 +0100

    feat: Leaderboard coloring for current user.

--- src/lib/web/leaderboard/PlayerRow.svelte
+++ src/lib/web/leaderboard/PlayerRow.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
 	import { generateAvatar } from '$lib/shared/utils/avatar.util';
+	import type { Player } from '@prisma/client';
 	import classNames from 'classnames';
+	import { getContext } from 'svelte';
 	import { Position, Winrate } from '.';
 	import type { PlayerInfo } from '../../../routes/+page.svelte';
 
 	export let player: PlayerInfo;
 	export let position: number;
+
+	const user = getContext<SvelteStore<Player>>('user');
 </script>
 
-<div class={classNames('text-center flex items-center px-3 py-2  rounded-full odd:bg-gray-900/20')}>
+<div
+	class={classNames('text-center flex items-center px-3 py-2 rounded-full odd:bg-gray-900/20', {
+		'!bg-emerald-600/30 text-emerald-200': $user?.id === player.id,
+	})}
+>
 	<div class="flex items-center w-2/3">
 		<div class="w-14">
 			<Position {position} />
@@ -28,6 +36,10 @@
 		</div>
 	</div>
 	<div class="w-1/3 tabular-nums hidden sm:flex flex-col gap-1">
-		<Winrate winrate={player.winrate} />
+		{#if $user?.id === player.id}
+			<Winrate winrate={player.winrate} colors={{ bg: 'bg-emerald-700', fg: 'bg-emerald-300' }} />
+		{:else}
+			<Winrate winrate={player.winrate} />
+		{/if}
 	</div>
 </div>
