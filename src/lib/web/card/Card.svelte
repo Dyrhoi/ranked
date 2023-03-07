46147129b0751467cfc8bfe5af6c4946c6520ab4
Dyrhoi <realarcoz@gmail.com>
2023-03-07 18:43:53 +0100

    feat: cooler hover effect on cards.

--- src/lib/web/card/Card.svelte
+++ src/lib/web/card/Card.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import classnames from 'classnames';
+	import { onMount } from 'svelte';
 	import { twMerge } from 'tailwind-merge';
 
 	let clazz: string = '';
@@ -8,9 +9,26 @@
 	export let border: 'inline' | 'external' = 'external';
 
 	export let clickAble = false;
+
+	let card: HTMLElement;
+	onMount(() => {
+		card.onmousemove = (e) => {
+			const { left, top, width, height } = card.getBoundingClientRect();
+			const x = e.clientX - left;
+			const y = e.clientY - top;
+
+			card.style.setProperty('--x', `${x}px`);
+			card.style.setProperty('--y', `${y}px`);
+		};
+
+		return () => {
+			card.onmousemove = null;
+		};
+	});
 </script>
 
 <div
+	bind:this={card}
 	class={classnames(
 		twMerge(
 			'bg-gradient-to-t from-white/5 to-white/0 card p-24 rounded-xl relative overflow-hidden',
@@ -41,26 +59,27 @@
 			background: linear-gradient(rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.07));
 			-webkit-mask: linear-gradient(black, black) content-box content-box,
 				linear-gradient(black, black);
+			mask-image: linear-gradient(black, black), linear-gradient(black, black);
 			-webkit-mask-composite: xor;
+			mask-composite: exclude;
 		}
 
 		&.clickable {
 			&::after {
 				transition: all 0.2s;
 				background-position: left;
-				background: linear-gradient(
-					to bottom right,
-					rgba(255, 255, 255, 0) 30%,
-					rgba(255, 255, 255, 0.02),
-					rgba(255, 255, 255, 0) 70%
+				background: radial-gradient(
+					400px circle at var(--x) var(--y),
+					rgba(255, 255, 255, 0.3),
+					transparent
 				);
-				background-size: 100% 200%;
 				opacity: 0;
+				mix-blend-mode: soft-light;
+				will-change: background, opacity;
 			}
 
 			&:hover {
 				&::after {
-					background-position: right;
 					opacity: 1;
 				}
 			}
